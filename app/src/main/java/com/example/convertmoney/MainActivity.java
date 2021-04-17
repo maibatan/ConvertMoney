3adf281464d579cbd6e95f18a14ab5018213714f
maibatan <mait88319@gmail.com>
2021-04-17 22:56:14 +0700

    Complete

--- app/src/main/java/com/example/convertmoney/MainActivity.java
+++ app/src/main/java/com/example/convertmoney/MainActivity.java
@@ -1,79 +1,299 @@
 package com.example.convertmoney;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
+import com.example.convertmoney.Adapter.ResultAdapter;
+import com.example.convertmoney.Model.Result;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
-    private Spinner spnCategory1;
-    private Spinner spnCategory2;
+    private Spinner spnFrom;
+    private Spinner spnTo;
+    private EditText value;
+    private Button complete;
+    private TextView clear;
+    private RecyclerView resultRecycleView;
+    private ProgressDialog progressDialog;
+    private HashMap<String,String> listCountryCode =new HashMap<>();
+    private List<String> listCountry = new ArrayList<>();
+    private List<Result> results= new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spnCategory1 = (Spinner) findViewById(R.id.spinner1);
-        spnCategory2 = (Spinner) findViewById(R.id.spinner2);
+        //Get view from activity
+        spnFrom = (Spinner) findViewById(R.id.spinnerFrom);
+        spnTo = (Spinner) findViewById(R.id.spinnerTo);
+        complete= (Button) findViewById(R.id.button);
+        value= (EditText) findViewById(R.id.value);
+        clear=(TextView) findViewById(R.id.clear);
+        resultRecycleView =(RecyclerView) findViewById(R.id.result) ;
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        resultRecycleView.setLayoutManager(layoutManager);
 
-        List<String> list1 = new ArrayList<>();
-        list1.add("1");
-        list1.add("2");
-        list1.add("3");
-        list1.add("4");
-        list1.add("5");
-
-        ArrayAdapter<String> adapter1 = new ArrayAdapter(this, android.R.layout.simple_spinner_item,list1);
-        adapter1.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
-
-        // set adaper 1
-        spnCategory1.setAdapter(adapter1);
-        spnCategory1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        //Set list country data
+        listCountry.add("None");
+        new GetDataCountry().execute();
 
 
+        //Set value into spinners
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, listCountry);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnFrom.setAdapter(adapter);
+        spnTo.setAdapter(adapter);
+
+
+        //Set invisible resutl when focus edittext
+        value.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this, spnCategory1.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus)
+                {
+                    resultRecycleView.setVisibility(View.INVISIBLE);
+
+                }
+                if(!hasFocus)
+                {
+                    InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(),0);
+                    resultRecycleView.setVisibility(View.VISIBLE);
+                }
             }
-
+        });
+        value.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId== EditorInfo.IME_ACTION_DONE)
+                {
+                    value.clearFocus();
 
+
+                }
+                return false;
             }
         });
 
-        // set adapter 2
-        List<String> list2 = new ArrayList<>();
-        list2.add("6");
-        list2.add("7");
-        list2.add("8");
-        list2.add("9");
-        list2.add("10");
-
-        ArrayAdapter<String> adapter2 = new ArrayAdapter(this, android.R.layout.simple_spinner_item,list2);
-        adapter2.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
-
-        spnCategory2.setAdapter(adapter2);
-        spnCategory2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-
+        //set Event clear history
+        clear.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this, spnCategory2.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
+            public void onClick(View v) {
+                resultRecycleView.setAdapter(null);
+                results.clear();
             }
         });
 
+        // set event when clicked button
+        complete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            if (!value.getText().toString().equals("")){
+                    if(!spnFrom.getSelectedItem().equals("None")&&!spnTo.getSelectedItem().equals("None"))
+                    {
+
+                        value.clearFocus();
+                        new GetDataCurrency().execute(listCountryCode.get(spnFrom.getSelectedItem().toString()));
+
+
+
+                    }
+                }
+            }
+        });
     }
+
+        // Get countries have data to exchange currency
+        protected class GetDataCountry extends AsyncTask<Void, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("Fetching conntry data");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            StringBuffer response = null;
+            try {
+                String path = "https://www.fxexchangerate.com/currency-converter-rss-feed.html";
+                URL url = new URL(path);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    // Reading response from input Stream
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String output;
+                    response = new StringBuffer();
+
+                    while ((output = in.readLine()) != null) {
+                        response.append(output);
+                    }
+                    in.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return response.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (progressDialog.isShowing())
+                progressDialog.dismiss();
+
+            Document document =  Jsoup.parse(s);
+            Element context =  document.getElementsByClass("rss").first();
+            List<Element> list=context.getElementsByTag("li");
+            for (int i=0; i<list.size();i++)
+            {
+                String url=list.get(i).getElementsByTag("a").first().attr("href");
+                String currencyName=list.get(i).text();
+                listCountry.add(currencyName);
+                listCountryCode.put(currencyName,url);
+            }
+
+
+
+
+        }
+    }
+
+
+    protected class GetDataCurrency extends AsyncTask<String,Void,String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("Fetching conntry data");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuffer response = null;
+            try {
+
+                URL url = new URL(strings[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    // Reading response from input Stream
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String output;
+                    response = new StringBuffer();
+
+                    while ((output = in.readLine()) != null) {
+                        response.append(output);
+                    }
+                    in.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return response.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (progressDialog.isShowing())
+                progressDialog.dismiss();
+
+
+            Document document =  Jsoup.parse(s);
+            Elements listItem=document.body().getElementsByTag("item");
+
+            for (int i=0;i<listItem.size();i++)
+            {
+
+                    String url=listItem.get(i).getElementsByTag("guid").first().text();
+
+                    if(listCountryCode.get(spnTo.getSelectedItem().toString()).contains(url))
+                    {
+                        String description=listItem.get(i).getElementsByTag("description").first().text();
+                        Pattern pattern = Pattern.compile("[0-9]{0,10}\\.[0-9]{0,10}");
+                        Matcher matcher = pattern.matcher(description);
+                        boolean matchFound = matcher.find();
+                        if(matchFound) {
+
+                            float resultFloat= Float.valueOf(matcher.group(0))*Float.valueOf(value.getText().toString());
+                            String currencyFrom=spnFrom.getSelectedItem().toString().split(" ")[0];
+                            String currencyTo=spnTo.getSelectedItem().toString().split(" ")[0];
+
+                            Result result=new Result(value.getText().toString(),String.valueOf(resultFloat),currencyFrom,currencyTo);
+                            results.add(result);
+                            ResultAdapter resultAdapter= new ResultAdapter(getBaseContext(),results);
+                            resultRecycleView.setAdapter(resultAdapter);
+                        }
+
+
+
+
+
+                        break;
+                    }
+
+
+            }
+
+
+
+
+
+        }
+    }
+
 
 }
---
+++ app/src/main/java/com/example/convertmoney/Model/Result.java
@@ -0,0 +1,40 @@
+package com.example.convertmoney.Model;
+
+import android.widget.TextView;
+
+public class Result {
+    String valueView,resultView;
+    String currencyFrom, currencyTo;
+
+    public Result(String valueView, String resultView, String currencyFrom, String currencyTo) {
+        this.valueView = valueView;
+        this.resultView = resultView;
+        this.currencyFrom = currencyFrom;
+        this.currencyTo = currencyTo;
+    }
+
+    public Result(Result result) {
+        this.valueView = result.valueView;
+        this.resultView = result.resultView;
+        this.currencyFrom = result.currencyFrom;
+        this.currencyTo = result.currencyTo;
+    }
+
+    public String getValueView() {
+        return valueView;
+    }
+
+    public String getResultView() {
+        return resultView;
+    }
+
+    public String getCurrencyFrom() {
+        return currencyFrom;
+    }
+
+    public String getCurrencyTo() {
+        return currencyTo;
+    }
+
+
+}
